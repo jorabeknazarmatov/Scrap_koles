1f7873b2bafa6582872ef4f95be665448d0dde1c
TURABEK NAZARMATOV <7916947090@my.lerna.uz>
2025-09-27 18:38:30 +0500

    Добавлен тайм зона для даты

--- conf.py
+++ conf.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+from zoneinfo import ZoneInfo
+from datetime import datetime
 
 
 load_dotenv()
@@ -13,5 +15,7 @@ LINKS = os.getenv('LINKS')
 
 links = [i for i in LINKS.split()]
 
-print(links)
 url = f'postgresql+psycopg://{USER}:{PASS}@{HOST}:{PORT}/{DBNAME}'
+
+# link = os.getenv("LINK")
+now_utc = datetime.now(ZoneInfo("UTC")).astimezone(ZoneInfo(os.getenv("TMZONE")))
